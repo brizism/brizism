22bfdb4f8b49e27d5c6b372acb35d71a35c99662
brizism <montielbriseida@gmail.com>
2018-05-23 19:30:43 -0400

    add gradients and sizes

---
+++ src/options/config.js
@@ -0,0 +1,38 @@
+export const COLORS = [
+
+]
+
+export const GRADIENTS = [
+  'vital-ocean',
+  'kale-salad',
+  'disco-club',
+  'shady-lane',
+  'retro-wagon',
+  'fresco-crush',
+  'cucumber-water',
+  'sea-salt',
+  'par-four',
+  'ooey-gooey',
+  'bloody-mimosa',
+  'lovely-lilly',
+  'aqua-spray',
+  'mello-yellow',
+  'dusty-cactus'
+]
+
+export const BUTTON_SIZE = [
+  'small',
+  'medium',
+  'large',
+  'wide',
+  'extra-wide',
+  'full-width'
+]
+
+export const HEADLINE_SIZE = [
+  'h1',
+  'h2',
+  'h3',
+  'h4',
+  'h5'
+]
