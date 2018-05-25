22fbbc96b20d494efd0472f2ab746a0c7db55788
brizism <montielbriseida@gmail.com>
2018-05-24 23:59:09 -0400

    display buttons and headlines

--- src/Brizism.js
+++ src/Brizism.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Button from './components/Button';
+import Headline from './components/Headline';
 
 class Brizism extends Component {
   render() {
     return (
       <div>
-       <Button>Click me!</Button>
+       <Button>Click here!</Button>
+       <Button gradient='retro-wagon' size='small'>Submit</Button>
+       <Button gradient='retro-wagon' size='medium'>Submit</Button>
+       <Button gradient='retro-wagon' size='large'>Submit</Button>
+       <Button gradient='retro-wagon' size='wide'>Submit</Button>
+       <Button gradient='retro-wagon' size='extra-wide'>Submit</Button>
+       <Button gradient='retro-wagon' size='full-width'>Submit</Button>
+       <Headline size='h1'>Header h1</Headline>
+       <Headline size='h2'>Header h2</Headline>
+       <Headline size='h3'>Header h3</Headline>
+       <Headline size='h4'>Header h4</Headline>
+       <Headline size='h5'>Header h5</Headline>
       </div>
     );
   }
